1d1873d608699d51364d6334c69f60d4d19d84a3
Balázs Kiss <dev@balazskiss.name>
2015-02-17 18:08:24 +0000

    iOS video support

--- Plugins/iOS/SAUnity.mm
+++ Plugins/iOS/SAUnity.mm
@@ -1,6 +1,5 @@
 //
-//  SAUnity.m
-//  Pods
+//  SAUnity.mm
 //
 //  Created by Balázs Kiss on 17/02/15.
 //
@@ -8,12 +7,15 @@
 
 
 #import <Foundation/Foundation.h>
+#import "SuperAwesome.h"
 
 extern "C" {
     
     void SuperAwesomeUnityMethod()
     {
-        NSLog(@"A macska fel van maszva a fara");
+        SAVideoAdViewController *vc = [[SAVideoAdViewController alloc] initWithAppID:@"14" placementID:@"314228"];
+        UIViewController *rvc = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [rvc presentViewController:vc animated:YES completion:nil];
     }
     
 }
