f006125caf96ed349a74457e0a763722e403c375
Balázs Kiss <dev@balazskiss.name>
2015-03-10 11:11:43 +0000

    Updated version number

--- Plugins/SuperAwesome.cs
+++ Plugins/SuperAwesome.cs
@@ -10,7 +10,7 @@ public class SuperAwesome : MonoBehaviour {
 	}
 
 	public static string getVersion(){
-		return "SuperAwesome Unity SDK version 1.0 ("+SABridge.getVersion()+")";
+		return "SuperAwesome Unity SDK version 1.1 ("+SABridge.getVersion()+")";
 	}
 
 	public static void setAppId(int appId){
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 SuperAwesome Unity SDK
 ==========================
 
+Version 1.1
+
 For more information check out the [SuperAwesome Developer Portal](http://developers.superawesome.tv/docs/unitysdk).
