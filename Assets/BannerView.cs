cc8435f59f4344649448c5e59fc46d0974ec1e15
Balázs Kiss <dev@balazskiss.name>
2015-06-17 12:21:56 +0100

    UNity code conventions

--- Assets/BannerView.cs
+++ Assets/BannerView.cs
@@ -8,8 +8,9 @@ namespace SuperAwesome
 {
 	public class BannerView : MonoBehaviour {
 
-		public String placementID = "Your Placement ID";
 		public enum Layout { Manual, Top, Bottom };
+
+		public String placementID = "Your Placement ID";
 		public Layout layout = Layout.Manual;
 
 		public delegate void BannerWasLoadedHandler();
@@ -20,49 +21,54 @@ namespace SuperAwesome
 		private Button button;
 		private Image image;
 		private Dictionary<string, object> ad;
-		
-		private Int64 _width;
-		private Int64 _height;
+		private Int64 width;
+		private Int64 height;
 
 		// Use this for initialization
-		void Start () {
-			StartCoroutine(loadAd());
+		void Start ()
+		{
+			StartCoroutine(Load());
 
 			this.image = this.GetComponent<Image>();
 
 			this.button = this.GetComponent<Button>();
 			this.button.onClick.AddListener (() => OnClick ());
 
-			align ();
-			hide ();
+			Align ();
+			Hide ();
 		}
 
 		// Update is called once per frame
-		void Update () {
+		void Update ()
+		{
 
 		}
 
-		private void show(){
+		private void Show()
+		{
 			this.image.color = Color.white;
 		}
 
-		private void hide(){
+		private void Hide()
+		{
 			this.image.color = Color.clear;
 		}
 
-		private void align(){
+		private void Align()
+		{
 			float x;
 			float y;
-			switch (this.layout) {
+			switch (this.layout)
+			{
 				case Layout.Bottom:
 					x = Screen.width / 2;
-					y = this._height / 2;
+					y = this.height / 2;
 					break;
 				case Layout.Top:
 					x = Screen.width / 2;
-					y = Screen.height - this._height/2;
+					y = Screen.height - this.height/2;
 					break;
-			default:
+				default:
 					x = transform.position.x;
 					y = transform.position.y;
 					break;
@@ -70,13 +76,14 @@ namespace SuperAwesome
 			transform.position = new Vector3 (x, y, transform.position.z);
 		}
 
-		private IEnumerator loadAd(){
+		private IEnumerator Load()
+		{
 			this.ad = SuperAwesome.instance.adManager.getAd (this.placementID);
 			Dictionary<string, object> creative = this.ad ["creative"] as Dictionary<string, object>;
 			Dictionary<string, object> details = creative ["details"] as Dictionary<string, object>;
 			string imgurl = (string) details["image"];
-			this._width = (Int64) details["width"];
-			this._height = (Int64) details["height"];
+			this.width = (Int64) details["width"];
+			this.height = (Int64) details["height"];
 			Debug.Log (imgurl);
 			
 			WWW image = new WWW(imgurl);
@@ -85,24 +92,22 @@ namespace SuperAwesome
 			Texture2D texture = image.texture;
 
 			//Resize button using its RectTransform component
-			this.button.image.rectTransform.sizeDelta = new Vector2 (this._width, this._height);
+			this.button.image.rectTransform.sizeDelta = new Vector2 (this.width, this.height);
 
 			//Create a new sprite with the texture and apply it to the button
 			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f));
 			this.button.image.sprite = sprite;
 
-			show ();
+			Show ();
 
 			if(OnBannerWasLoaded != null) OnBannerWasLoaded();
 		}
 
-		private void OnClick(){
-			Debug.Log ("KLIKK");
-
+		private void OnClick()
+		{
 			this.ad = SuperAwesome.instance.adManager.getAd (this.placementID);
 			Dictionary<string, object> creative = this.ad ["creative"] as Dictionary<string, object>;
 			String clickURL = creative ["click_url"] as String;
-			Debug.Log (clickURL);
 			Application.OpenURL(clickURL);
 
 			if(OnBannerWasClicked != null) OnBannerWasClicked();
