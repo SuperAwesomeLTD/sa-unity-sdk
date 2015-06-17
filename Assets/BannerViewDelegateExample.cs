63459e36125bcab6c679ca7007bc64c1dd3bd9a9
Balázs Kiss <dev@balazskiss.name>
2015-06-17 11:39:16 +0100

    Delelgate example

---
+++ Assets/BannerViewDelegateExample.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using SuperAwesome;
+
+public class BannerViewDelegateExample : MonoBehaviour {
+
+	void OnEnable()
+	{
+		BannerView banner = this.GetComponent<BannerView>();
+		banner.OnBannerWasLoaded += OnLoad;
+		banner.OnBannerWasClicked += OnClick;
+	}
+	
+	
+	void OnDisable()
+	{
+		BannerView banner = this.GetComponent<BannerView>();
+		banner.OnBannerWasLoaded -= OnLoad;
+		banner.OnBannerWasClicked -= OnClick;
+	}
+
+	void OnLoad()
+	{
+		Debug.Log ("Banner Loaded");
+	}
+	
+	
+	void OnClick()
+	{
+		Debug.Log ("Banner Clicked");
+	}
+}
