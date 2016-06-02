d315e6679f74c59f94e055994ba715ef6fe35f39
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-06-02 18:03:40 +0100

    3.1.4

--- demo/Assets/SuperAwesome/Classes/SuperAwesome.cs
+++ demo/Assets/SuperAwesome/Classes/SuperAwesome.cs
@@ -50,7 +50,7 @@ namespace SuperAwesome {
 
 		/** functions to get info about the current SDK */
 		private string getVersion(){
-			return "3.1.2";
+			return "3.1.4";
 		}
 
 		private string getSdk() {
