87e1bf9b21bc35e438adcbd42f53d29430dadb8b
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-05-13 18:37:56 +0100

    Update

---
+++ demo/Assets/SuperAwesome/Classes/AwesomeAds.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tv {
+	namespace superawesome {
+		namespace sdk {
+			namespace publisher {
+
+				public class AwesomeAds {
+
+#if (UNITY_IPHONE && !UNITY_EDITOR)
+					[DllImport ("__Internal")]
+					private static extern void SuperAwesomeUnityAwesomeAdsInit (bool loggingEnabled);
+#endif
+
+					public static void init (bool loggingEnabled) {
+#if (UNITY_IPHONE && !UNITY_EDITOR)
+						var loggingEnabledL = loggingEnabled;
+						AwesomeAds.SuperAwesomeUnityAwesomeAdsInit (loggingEnabledL);
+#else
+						Debug.Log ("Initialising SDK");
+#endif
+					}
+				}
+			}
+		}
+	}
+}
+
