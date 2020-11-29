82ba72e9cfa63a6cdb8feefaccdc0aef8e2c077d
Gunhan Sancar <cgnhnc@gmail.com>
2020-11-29 17:18:03 +0000

    Update

--- scripts/refactor/build.sh
+++ scripts/refactor/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -ex
 
-./1.download-android.sh
-./2.download-ios.sh
-./3.download-unity.sh
-./4.build-android.sh
-./5.build-ios.sh
+
+# ./3.download-unity.sh
 ./6.build-unity.sh
 ./7.build-admob-unity.sh
 ./8.build-base-unity.sh
