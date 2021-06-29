90ac2a85320529c2a73d8f23200ca5c431e5a7ed
Danesh <danesh.harjani@superawesome.com>
2021-06-29 16:30:10 +0100

    added version and branch flags to scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,40 +1,63 @@
 #!/bin/bash -ex
 
-############################################################ 
-# step 0: get version flag
 ############################################################
-while getopts v: flag
-do 
-  case "${flag}" in
+# step 0: get flags
+############################################################
+while getopts "v:p:t:b:" arg
+do
+  case "${arg}" in
     v) version=${OPTARG};;
+  esac
+  case "${arg}" in
+    t) tag=${OPTARG};;
+  esac
+  case "${arg}" in
+    p) platform=${OPTARG};;
+  esac
+  case "${arg}" in
+    b) branch=${OPTARG};;
   esac
 done
 
-############################################################ 
+if [[ $branch == '' ]]
+then
+  branch='master'
+fi
+
+if [[ $platform == '' || ($platform != 'android' && $platform != 'ios' && $platform != 'all') ]]
+then
+  platform='all'
+fi
+
+############################################################
 # step 1: download the iOS, Android & Unity source code
 ############################################################
 
-./0.download.sources.sh
+./0.download.sources.sh -b $branch -p $platform
 
-############################################################ 
+############################################################
 # step 2: Build Android & move result to correct build folder
 ############################################################
 
-./1.prepare.android.sh 
-
-############################################################ 
+if [[ $platform == 'android' || $platform == 'all' ]]
+then
+  ./1.prepare.android.sh
+fi
+############################################################
 # step 3: Build iOS & move result to correct build folder
 ############################################################
 
-./2.prepare.ios.sh
-
-############################################################ 
+if [[ $platform == 'ios' || $platform == 'all' ]]
+then
+  ./2.prepare.ios.sh
+fi
+############################################################
 # step 4: Build Unity & move result to correct build folder
 ############################################################
 
-./3.build.unity.sh
+./3.build.unity.sh -p $platform
 
-############################################################ 
+############################################################
 # step 4: cleanup folders
 ############################################################
 
